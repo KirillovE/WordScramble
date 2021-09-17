c27cb16fc95e0378beb432c35697577934c3cdcf
Evgeniy Kirillov <eskirillov@avito.ru>
2021-09-17 08:05:13 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Евгений Кириллов on 17.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
